ac05635d044e93b7641cc59fb2f16ec0d3cddd2c
jamiejamiebobamie <jmccrory@vt.edu>
2019-02-26 15:08:24 -0800

    added jquery line for bootstrap

--- public/js/js_cookie.js
+++ public/js/js_cookie.js
@@ -5,7 +5,8 @@
  * Copyright 2006, 2015 Klaus Hartl & Fagner Brack
  * Released under the MIT license
  */
-;(function (factory) {
+// ;(function (factory) {
+function (factory) {
 	var registeredInModuleLoader = false;
 	if (typeof define === 'function' && define.amd) {
 		define(factory);
