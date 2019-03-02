62b10757947f047663c43ed51ec9b43ce1032e4f
ilikecoffee18 <cherish.kim@students.makeschool.com>
2019-03-02 15:52:50 -0800

    beautified everything to prevent merge conflicts based on syntax

--- models/resource.js
+++ models/resource.js
@@ -2,14 +2,39 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 const ResourceSchema = new Schema({
-    resource: {type: String, required: true}, // BB: This is the title
-    link: {type: String, required: false}, // JM: outdated version(pre-user version). CK: not required b/c some resources, like action items, won't have a link
-    user: {type: String, required: false},
-    time: {type: String, required: false},
-    flagged: {type: Boolean, required: false},
-    ignoreFlag: {type: Boolean, required: false},
-    mood: {type: Array, required: true}, // CK: Why an array? Well, we might find a resource that's good for both, say, anger and sadness. Being able to "tag" media for plural moods would be useful.
-    listId: {type: Schema.Types.ObjectId, ref: 'List', required: false} // CK: Resources deep-copied onto a List should be associated with that List. Not every Resource is going to be a deep copy, so not every Resource needs listId. That's why little object listId is set to "required: false".
+    resource: {
+        type: String,
+        required: true
+    }, // BB: This is the title
+    link: {
+        type: String,
+        required: false
+    }, // JM: outdated version(pre-user version). CK: not required b/c some resources, like action items, won't have a link
+    user: {
+        type: String,
+        required: false
+    },
+    time: {
+        type: String,
+        required: false
+    },
+    flagged: {
+        type: Boolean,
+        required: false
+    },
+    ignoreFlag: {
+        type: Boolean,
+        required: false
+    },
+    mood: {
+        type: Array,
+        required: true
+    }, // CK: Why an array? Well, we might find a resource that's good for both, say, anger and sadness. Being able to "tag" media for plural moods would be useful.
+    listId: {
+        type: Schema.Types.ObjectId,
+        ref: 'List',
+        required: false
+    } // CK: Resources deep-copied onto a List should be associated with that List. Not every Resource is going to be a deep copy, so not every Resource needs listId. That's why little object listId is set to "required: false".
 });
 
 module.exports = mongoose.model('Resource', ResourceSchema);
--- models/user.js
+++ models/user.js
@@ -2,38 +2,48 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const Schema = mongoose.Schema;
 
- const UserSchema = new Schema({
-   createdAt: { type: Date },
-   updatedAt: { type: Date },
-   username: { type: String, required: true },
-   password: { type: String, required: true }
- });
+const UserSchema = new Schema({
+  createdAt: {
+    type: Date
+  },
+  updatedAt: {
+    type: Date
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
 
- UserSchema.pre("save", function(next) {
-   // SET createdAt AND updatedAt
-   const now = new Date();
-   this.updatedAt = now;
-   if (!this.createdAt) {
-     this.createdAt = now;
-   }
+UserSchema.pre("save", function (next) {
+  // SET createdAt AND updatedAt
+  const now = new Date();
+  this.updatedAt = now;
+  if (!this.createdAt) {
+    this.createdAt = now;
+  }
 
-   // ENCRYPT PASSWORD
-   const user = this;
-   if (!user.isModified("password")) {
-     return next();
-   }
-   bcrypt.genSalt(10, function(err, salt) {
-     bcrypt.hash(user.password, salt, function(err, hash) {
-       user.password = hash;
-       next();
-     });
-   });
- });
+  // ENCRYPT PASSWORD
+  const user = this;
+  if (!user.isModified("password")) {
+    return next();
+  }
+  bcrypt.genSalt(10, function (err, salt) {
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      user.password = hash;
+      next();
+    });
+  });
+});
 
- UserSchema.methods.comparePassword = function(password, done) {
-   bcrypt.compare(password, this.password, function(err, isMatch) {
-     done(err, isMatch);
-   });
- };
+UserSchema.methods.comparePassword = function (password, done) {
+  bcrypt.compare(password, this.password, function (err, isMatch) {
+    done(err, isMatch);
+  });
+};
 
- module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model("User", UserSchema);
