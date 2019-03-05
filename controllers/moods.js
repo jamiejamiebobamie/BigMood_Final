c4e7014e10156004eee89f8f9aea9f4210cb4619
betsybaileyy <betsy.bailey80@gmail.com>
2019-03-05 12:28:54 -0800

    route to bored resource page working

--- controllers/moods.js
+++ controllers/moods.js
@@ -23,4 +23,25 @@ app.get('/angry', (req, res) => {
       })
   })
 });
+app.get('/bored', (req, res) => {
+  var currentUser = req.user;
+  Resource.count({
+    mood: 'Bored'
+  }).exec(function (err, count) {
+    // "Get a random entry"
+    var random = Math.floor(Math.random() * count)
+    // "Again query all users but only fetch one offset by our random #""
+    Resource.findOne({
+      mood: 'Bored'
+    }).skip(random).exec(
+      function (err, result) {
+        // "Tada! random user"
+        console.log(result)
+        res.render('bored', {
+          resource: result,
+          currentUser
+        });
+      })
+  })
+});
 }
