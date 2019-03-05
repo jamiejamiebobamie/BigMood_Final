ada0705cda244bba419e01b6a9eb6aa24d20b839
ilikecoffee18 <cherish.kim@students.makeschool.com>
2019-03-05 12:45:48 -0800

    all routes for moods now functional

--- controllers/moods.js
+++ controllers/moods.js
@@ -2,46 +2,101 @@ const Resource = require('../models/resource');
 
 module.exports = function (app) {
 
-app.get('/angry', (req, res) => {
-  var currentUser = req.user;
-  Resource.count({
-    mood: 'Angry'
-  }).exec(function (err, count) {
-    // "Get a random entry"
-    var random = Math.floor(Math.random() * count)
-    // "Again query all users but only fetch one offset by our random #""
-    Resource.findOne({
+  app.get('/angry', function (req, res) {
+    var currentUser = req.user;
+    Resource.count({
       mood: 'Angry'
-    }).skip(random).exec(
-      function (err, result) {
-        // "Tada! random user"
-        console.log(result)
-        res.render('angry', {
-          resource: result,
-          currentUser
-        });
-      })
-  })
-});
-app.get('/bored', (req, res) => {
-  var currentUser = req.user;
-  Resource.count({
-    mood: 'Bored'
-  }).exec(function (err, count) {
-    // "Get a random entry"
-    var random = Math.floor(Math.random() * count)
-    // "Again query all users but only fetch one offset by our random #""
-    Resource.findOne({
+    }).exec(function (err, count) {
+      // "Get a random entry"
+      var random = Math.floor(Math.random() * count)
+      // "Again query all users but only fetch one offset by our random #""
+      Resource.findOne({
+        mood: 'Angry'
+      }).skip(random).exec(
+        function (err, result) {
+          // "Tada! random"
+          console.log(result)
+          res.render('angry', {
+            resource: result,
+            currentUser
+          });
+        })
+    })
+  });
+
+  app.get('/bored', function (req, res) {
+    var currentUser = req.user;
+    Resource.count({
       mood: 'Bored'
-    }).skip(random).exec(
-      function (err, result) {
-        // "Tada! random user"
-        console.log(result)
-        res.render('bored', {
-          resource: result,
-          currentUser
-        });
-      })
-  })
-});
+    }).exec(function (err, count) {
+      var random = Math.floor(Math.random() * count)
+      Resource.findOne({
+        mood: 'Bored'
+      }).skip(random).exec(
+        function (err, result) {
+          console.log(result)
+          res.render('bored', {
+            resource: result,
+            currentUser
+          });
+        })
+    })
+  });
+
+  app.get('/frustrated', function (req, res) {
+    var currentUser = req.user;
+    Resource.count({
+      mood: 'Frustrated'
+    }).exec(function (err, count) {
+      var random = Math.floor(Math.random() * count)
+      Resource.findOne({
+        mood: 'Frustrated'
+      }).skip(random).exec(
+        function (err, result) {
+          console.log(result)
+          res.render('frustrated', {
+            resource: result,
+            currentUser
+          });
+        })
+    })
+  });
+
+  app.get('/lonely', function (req, res) {
+    var currentUser = req.user;
+    Resource.count({
+      mood: 'Lonely'
+    }).exec(function (err, count) {
+      var random = Math.floor(Math.random() * count)
+      Resource.findOne({
+        mood: 'Lonely'
+      }).skip(random).exec(
+        function (err, result) {
+          console.log(result)
+          res.render('lonely', {
+            resource: result,
+            currentUser
+          });
+        })
+    })
+  });
+
+  app.get('/sad', function (req, res) {
+    var currentUser = req.user;
+    Resource.count({
+      mood: 'Sad'
+    }).exec(function (err, count) {
+      var random = Math.floor(Math.random() * count)
+      Resource.findOne({
+        mood: 'Sad'
+      }).skip(random).exec(
+        function (err, result) {
+          console.log(result)
+          res.render('sad', {
+            resource: result,
+            currentUser
+          });
+        })
+    })
+  });
 }
